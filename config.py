1ca4cff00ad5289fd54ce34c0ba411369170d94c
MandelaK <kazi.keith@gmail.com>
2018-12-22 12:59:52 +0300

    chore: set up database
    set up SQLite database using flask-sqlalchemy and configured it. Also added migration engine using flask-migrate.
    
    [#chapter4]

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config():
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or 'asoianna09leou098qla;dfuap98p2qklda8ea'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCEMY_TRACK_MODIFICATIONS = False
